1ffafa28753e8e82b0149b3ea47e1e9c24059adc
Yassine El Garras <yassine7500@gmail.com>
2019-07-04 17:56:41 +0200

    Initial Commit

---
+++ prueba_tecnica/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  prueba_tecnica
+//
+//  Created by Yassine El garras on 04/07/2019.
+//  Copyright © 2019 Yassine El garras. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
